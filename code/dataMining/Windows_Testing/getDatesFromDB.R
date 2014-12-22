2562e350b60a2e324eafc4e3bd38f9192fa03287
eliz.thf@gmail.com <eliz.thf@gmail.com@237f01b3-efeb-0e81-b8c9-b299af5011e9>
2014-12-22 08:38:21 +0000

    for the latest DB with 30+ columns, use runThis.R

---
+++ code/dataMining/Windows_Testing/getDatesFromDB.R
@@ -0,0 +1,54 @@
+library(RODBC);
+
+getFirstBizDateEachMonth <- function(startDateIncluded, endDateExcluded) {
+  channel <- odbcConnect("localDB") 
+  cal.dates <- sqlQuery(channel, paste0("
+                         SELECT *
+                         FROM [S&P].[dbo].[tb_Calendar]
+                         WHERE [CalendarDate] >= '", startDateIncluded, 
+                         "' AND [CalendarDate] < '", endDateExcluded,
+                         "' order by [CalendarDate]"))
+  as.character(cal.dates[cal.dates$IsBizMonthBeg == 1,"CalendarDate"])
+}
+
+# xx = getFirstBizDateEachMonth('2008-01-01', '2010-01-01')
+
+getAllDistinctDatesFactorScore <- function(startDateIncluded, endDateExcluded) {
+  channel <- odbcConnect("localDB") 
+  cal.dates <- sqlQuery(channel, 
+                          paste0("Select Distinct [Date]
+                          FROM [S&P].[dbo].[tb_FactorScore]
+                          where [Date] >= '", startDateIncluded, 
+                          "' AND [Date] < '", endDateExcluded,
+                          "' order by [Date]"
+                          ))
+  return(as.character(cal.dates[,1]))
+}
+# allDistinctDates = getAllDistinctDatesFactorScore('2008-01-01', '2010-01-01')
+
+getDates_41bizdays <- function(testdate) {
+  rowNo <- which(allDistinctDates == testdate)-41
+  as.character(allDistinctDates[rowNo:(rowNo+40),]) #so not inclusive of testdate itself
+}
+
+# getTrainingDates_41bizdaysb4('2008-01-02')
+
+getMomDate <- function(testdate) {
+  as.character(allDistinctDates[which(allDistinctDates == testdate)+20,])
+}
+get21DaysB4 <- function(testdate) {
+  as.character(allDistinctDates[which(allDistinctDates == testdate)-21,])
+}
+
+getMomDates <- function(trainingDates) {
+  apply(data.frame(trainingDates), 1, getMomDate)
+}
+# getStartDate_41bizbaysb4 <- function(testdate) {
+#   as.character(allDistinctDates[which(allDistinctDates == testdate)-41,])
+# }
+# # getStartDate_41bizbaysb4('2008-01-02') #"2007-11-06 MYT"
+# 
+# GetMomEndDate_40DaysFromStart <- function(startDate) {
+#   as.character(allDistinctDates[which(allDistinctDates == startDate)+40,])
+# }
+# # GetMomEndDate_40DaysFromStart("2007-11-06 MYT") #"2008-01-01 MYT"
